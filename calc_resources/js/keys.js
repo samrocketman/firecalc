6f8aca612cd02ff57ebc25ae8604bb6d834ee44f
Sam Gleske <sam.mxracer@gmail.com>
2015-06-05 23:06:33 -0400

    firecalc 1.2.6

--- calc_resources/js/keys.js
+++ calc_resources/js/keys.js
@@ -6,176 +6,168 @@
 \*********************************************/
 function keydown(e)
 {
-	var keyCode;
-	if(msie)
+	//grab the key event, if no event able to be grabbed then exit the function
+	var keyCode=(msie)?event.keyCode:(window.event)?window.event.keyCode:(e)?e.which:false;
+	if(!keyCode)
+		return false;
+	
+	//detect the keys pressed
+	switch(keyCode)
 	{
-		keyCode=event.keyCode;
-		if (keyCode == 13 || keyCode == 61)
-		{//Enter Key
-			calc.enter.focus();
-			return compute();
-		}
-	}
-	else
-	{
-		if(window.event)
-			keyCode=window.event.keyCode;
-		else if(e)
-			keyCode=e.which;
-		
-		if(keyCode==13||keyCode==61)
-		{//EnterKey
+		case 8:
+			//BackspaceKey
+			deleteChar();
+			calc.backbtn.focus();
+			break;
+		case 13:
+		case 61:
+			//EnterKey
 			calc.enter.focus();
 			if(checkNum(display.value))
 			{
-				return compute(calc);
+				compute(calc);
 			}
-		}
+			break;
+		case 46:
+			//DeleteKey
+			display.value=0;
+			calc.delbtn.focus();
+			break;
+		default:
+			return false;
+			break;
 	}
-	if(keyCode==46)
-	{//DeleteKey
-		display.value=0;
-		document.getElementById("delbtn").focus();
-	}
-	if(keyCode==8)
-	{//BackspaceKey
-		deleteChar();
-		document.getElementById("backbtn").focus();
-	}
+	// Every time the '/' key is used in firefox a search comes up
+	// This grabs focus away from the search so the calculator can still be used
+	if(firefox)
+		setTimeout("window.focus()",1);
 }
 
 
 
 function keypress(e)
 {
-	var keyCode;
-	if(msie)
-		keyCode=event.keyCode;
-	else
+	//grab the key event, if no event able to be grabbed then exit the function
+	var keyCode=(msie)?event.keyCode:(window.event)?window.event.keyCode:(e)?e.which:false;
+	if(!keyCode)
+		return false;
+	
+	//detect the keys pressed
+	switch(keyCode)
 	{
-		if(window.event)keycode=window.event.keyCode;
-		else if(e)
-			keyCode=e.which;
+		case 40:
+			calc.parl.focus();
+			addChar('(');
+			break;
+		case 41:
+			calc.parr.focus();
+			addChar(')');
+			break;
+		case 42:
+			calc.multiply.focus();
+			addChar('*');
+			break;
+		case 43:
+			calc.add.focus();
+			addChar('+');
+			break;
+		case 44:
+			addChar(',');
+			break;
+		case 45:
+			calc.subtract.focus();
+			addChar('-');
+			break;
+		case 46:
+			calc.dot.focus();
+			addChar('.');
+			break;
+		case 47:
+			calc.divide.focus();
+			addChar('/');
+			break;
+		case 48:
+			calc.btn0.focus();
+			addChar('0');
+			break;
+		case 49:
+			calc.btn1.focus();
+			addChar('1');
+			break;
+		case 50:
+			calc.btn2.focus();
+			addChar('2');
+			break;
+		case 51:
+			calc.btn3.focus();
+			addChar('3');
+			break;
+		case 52:
+			calc.btn4.focus();
+			addChar('4');
+			break;
+		case 53:
+			calc.btn5.focus();
+			addChar('5');
+			break;
+		case 54:
+			calc.btn6.focus();
+			addChar('6');
+			break;
+		case 55:
+			calc.btn7.focus();
+			addChar('7');
+			break;
+		case 56:
+			calc.btn8.focus();
+			addChar('8');
+			break;
+		case 57:
+			calc.btn9.focus();
+			addChar('9');
+			break;
+		case 67:
+		case 99:
+			//clear the memory when C is pressed (button MC)
+			calc.mClear.focus();
+			ClearMem();
+			break;
+		case 69:
+		case 101:
+			//insert e when E key is pressed
+			calc.ekey.focus();
+			addChar('e');
+			break;
+		case 77:
+		case 109:
+			//add to memory when M is pressed (button M+)
+			calc.mPlus.focus();
+			MemPlus();
+			break;
+		case 80:
+		case 112:
+			//insert pi when P is pressed
+			calc.pi.focus();
+			addChar('pi');
+			break;
+		case 82:
+		case 114:
+			//recall the memory when R is pressed (button MR)
+			calc.mRecall.focus();
+			RecallMem();
+			break;
+		case 83:
+		case 115:
+			//store the memory when S is pressed (button MS)
+			calc.mStore.focus();
+			StoreMem();
+			break;
+		default:
+			return false;
+			break;
 	}
-	if(keyCode==40)
-	{
-		calc.parl.focus();
-		addChar('(');
-	}
-	if(keyCode==41)
-	{
-		calc.parr.focus();
-		addChar(')');
-	}
-	if(keyCode==42)
-	{
-		calc.multiply.focus();
-		addChar('*');
-	}
-	if(keyCode==43)
-	{
-	calc.add.focus();
-	addChar('+');
-	}
-	if(keyCode==44)
-		addChar(',');
-	if(keyCode==45)
-	{
-		calc.subtract.focus();
-		addChar('-');
-	}
-	if(keyCode==46)
-	{
-		calc.dot.focus();
-		addChar('.');
-	}
-	if(keyCode==47)
-	{
-		calc.divide.focus();
-		addChar('/');
-	}
-	if(keyCode==48)
-	{
-		calc.btn0.focus();
-		addChar('0');
-	}
-	if(keyCode==49)
-	{
-		calc.btn1.focus();
-		addChar('1');
-	}
-	if(keyCode==50)
-	{
-		calc.btn2.focus();
-		addChar('2');
-	}
-	if(keyCode==51)
-	{
-		calc.btn3.focus();
-		addChar('3');
-	}
-	if(keyCode==52)
-	{
-		calc.btn4.focus();
-		addChar('4');
-	}
-	if(keyCode==53)
-	{
-		calc.btn5.focus();
-		addChar('5');
-	}
-	if(keyCode==54)
-	{
-		calc.btn6.focus();
-		addChar('6');
-	}
-	if(keyCode==55)
-	{
-		calc.btn7.focus();
-		addChar('7');
-	}
-	if(keyCode==56)
-	{
-		calc.btn8.focus();
-		addChar('8');
-	}
-	if(keyCode==57)
-	{
-		calc.btn9.focus();
-		addChar('9');
-	}
-	if(keyCode==59)
-	{
-		addChar(';');
-	}
-	if(keyCode==99||keyCode==67)
-	{//clear the memory when C is pressed (button MC)
-		calc.mClear.focus();
-		ClearMem();
-	}
-	if(keyCode==101||keyCode==69)
-	{//insert e when E key is pressed
-		calc.ekey.focus();
-		addChar('e');
-	}
-	if(keyCode==109||keyCode==77)
-	{//add to memory when M is pressed (button M+)
-		calc.mPlus.focus();
-		MemPlus();
-	}
-	if(keyCode==112||keyCode==80)
-	{//insert pi when P is pressed
-		calc.pi.focus();
-		addChar('pi');
-	}
-	if(keyCode==114||keyCode==82)
-	{//recall the memory when R is pressed (button MR)
-		calc.mRecall.focus();
-		RecallMem();
-	}
-	if(keyCode==115||keyCode==83)
-	{//store the memory when S is pressed (button MS)
-		calc.mStore.focus();
-		StoreMem();
-	}
+	// Every time the '/' key is used in firefox a search comes up
+	// This grabs focus away from the search so the calculator can still be used
+	if(firefox)
+		setTimeout("window.focus()",1);
 }
